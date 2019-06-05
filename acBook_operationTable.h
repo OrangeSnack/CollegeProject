2e8a77e3b1d31c33c7c4db3b0089050a46476118
slient13 <sjyhc25@gmail.com>
2019-06-06 00:05:16 +0900

    테이블 파트 명세부 작성
    - 테이블 구조 제작
    - 함수 명세부 제작, 정의부 미작성
    - 명령어 해석부 기본세팅(미완성)

--- acBook_operationTable.h
+++ acBook_operationTable.h
@@ -17,54 +17,99 @@
 #ifndef acBook_operationTable
     #define acBook_operationTable
 
-    typedef struct _BplusTree {
-        char* name_tree;    // 트리의 이름을 가리킬 포인터
-        Node* root;         // 뿌리 노드를 가리키는 포인터
-        Node* first_leaf;   // 시작 잎 노드를 가리키는 포인터
-        Node* pointer;      // 검색 등에서 사용되는 변동 포인터.
-        Record* name_col;   // 해당 테이블의 열 이름들을 모아둔 레코드 포인터.
-        int col;            // 검색 등에서 기준이 되는 column 번호.
-        int direction;      // 정렬 방향. 0 : 오름차순, 1 : 내림차순
-    } BplusTree;
+    #include "acBook_processingData.h"      // 테이블 연산 파일
 
-    // 생성함수 : BplusTree
-    void init_BplusTree (BplusTree* target);        
-    // 소멸함수 : BplusTree
-    void destroy_BplusTree (BplusTree* target);     
-    // 변환 : 문자열로 입력된 열 이름 -> 열 번호
-    int convert_col_to_num(BplusTree* target, char* col_name);
-    // 레코드 검색 : 일치하는 값이 있으면 해당 값을, 없으면 NULL을 반환.
-    char* search (BplusTree* target, int col, char* string);    
-    // 레코드 삽입 : 
-    int insert(BplusTree* target, int col, Record data);        
-    // 변환 : 'B+tree' -> 2차원 문자열 배열.
-    char*** convert_to_arr (BplusTree* target);     
+    typedef struct _Order_queue Order_queue;
 
+    // 테이블 구조체
+    typedef struct _Table {
+        char* name;         // 테이블의 이름
+        Col* start_col;     // 시작열을 가리키는 포인터.
+        int num_col;        // 열 갯수
+        int num_record;     // 행 갯수.
+        Cursor* cursor;     // 커서 포인터
+    } Table;
 
-    // 'record'를 정돈하고 모아 관리하는 'node'구조체. 'B+tree'의 일부.
-    typedef struct _Node {
-        char** data;        // 문자열 배열을 동적 할당 받을 2차원 char형 포인터
-        Node** child;       // 자식 노드들을 가리킬 포인터 배열을 받을 2차원 구조체 포인터, leaf 노드는 다음 노드만를 가리킴.
-        int position;       // 위치가 어디인지 확인. index : 0, leaf : 1;
-        int num_data;       // 해당 노드에 몇개의 데이터가 저장되어 있는지 기록.
-    } Node;
-
-    void init_Node (Node* target);                  // 생성함수 : Node
-    void destroy_Node (Node* target);               // 소멸함수 : Node
+    // 조작 명령을 받아 테이블 연산을 실행해 반환.
+    Table* table_manager(Order_queue* order);
+    // 테이블 생성 함수.
+    // name : 생성할 테이블의 이름
+    Table* new_table(char* name);
+    // 테이블 삭제 함수.
+    // target : 삭제할 테이블 객체의 주소
+    void delete_table(Table* target);
 
 
-    // 테이블의 각 자료를 구성하는 단방향 연결 리스트
-    typedef struct _Record {
+    // 열 구조체
+    typedef struct _Col {
+        char* name;         // 해당 열의 이름.
+        Col* next;          // 다음열을 가리키는 포인터. 끝 열이면 NULL
+        Col* prev;          // 이넞열을 가리키는 포인터. 시작 열이면 NULL
+        Data* start_data;   // 해당 열의 시작행을 가리키는 포인터
+        int is_primaryKey;  // 해당 열이 기준 열(primary key)인지 확인. 0 : 아님, 1 : 맞음.
+    } Col;
+
+    // 열 생성 함수.
+    // 생성 위치는 항상 마지막 열 다음.
+    // 레코드가 있는 상태에서 열을 추가시키면, 그 열의 데이터는 NULL 으로 초기화.
+    // name : 새로 생성할 열의 이름.
+    Col* new_col(Cursor* cursor, char* name);
+    // 열 삭제 함수.
+    // name : 삭제할 열의 이름
+    void delete_col(Cursor* cursor, char* name);
+    // 열 이동 함수.
+    // 커서가 가리키고 있는 열을 좌, 우 일정칸 움직임. 실행 후 0을 반환.
+    // 만약 이동할 공간이 불충분하다면 실행하지 않고 -1을 반환.
+    int move_col(Cursor* cursor, int move_length);
 
 
-    } Record;
+    // 데이터 구조체. 
+    // 데이터 관련 조작은 단일 데이터 수정 제외하곤 전부 레코드 단위로 동작.
+    typedef struct _Data {
+        char* contents;     // 해당 위치에 기록된 데이터 내용.
+        Data* next;         // 다음 레코드 데이터. 끝이면 NULL.
+        Data* prev;         // 이전 레코드 데이터. 시작이면 NULL.
+    } Data;
+
+    // 레코드 생성 함수.
+    // content : 레코드에 삽입될 문자열 배열
+    Data* new_record(Cursor* cursor, char** content);
+    // 레코드 삭제 함수 : 커서가 가리키고 있는 레코드를 삭제.
+    void delete_record(Cursor* cursor);
+    // 데이터 수정 함수 : 커서가 가리키고 있는 특정 데이터의 값을 변경.
+    // content : 기존 값을 대체할 문자열.
+    void edit_data(Cursor* cursor, char* content);
+    // 데이터 전체 수정 함수 : 특정열에서 특정 값을 가진 모든 데이터를 변경.
+    // col_name : 해당 함수를 적용할 열의 이름
+    // before : 바뀌기 전 값.
+    // content : 바꿀 값.
+    void edit_allData(Cursor* cursor, char* col_name, char* before, char* content);
+    // 데이터 정렬 함수.
+    // col_name : 정렬기준이 될 열 이름 배열. 낮은 인덱스가 가장 우선시 됨.
+    // direction : 정렬 방향. 1 : 오름차순, -1 : 내림차순
+    void sort_data(Cursor* cursor, char** col_name, int direction);
+
+
+    // 검색, 삽입, 삭제 등을 시행할 때 관측하고 있는 위치를 기록하는 구조체
+    // 테이블 객체와 같이 생성되었다가, 같이 삭제됨.
+    typedef struct _Cursor {
+        Table*  pos_table;  // 현재 가리키고 있는 데이터의 테이블 주소
+        Col*    pos_col;    // 현재 가리키고 있는 데이터의 열 주소
+        Data**  pos_record; // 현재 가리키고 있는 레코드의 전체 데이터 주소.
+    } Cursor;
+
+    // 커서 생성 함수.
+    void new_cursor();
+    // 커서 제거 함수.
+    void delete_cursor();
+    // 커서 이동 함수 : 지정한 열로 이동
+    // col_name : 이동할 열의 이름
+    Cursor* move_cursor_by_colName(char* col_name);
+    // 커서 이동 함수 : 열 이동.
+    // direction : 이동할 방향. 1 = 다음, -1 = 이전.
+    Cursor* move_cursor_col(int direction);
+    // 커서 이동 함수 : 행(레코드) 이동
+    // direction : 이동할 방향. 1 = 다음, -1 = 이전.
+    Cursor* move_cursor_record(int direction);
 
 #endif
-
-/* 
-세부 설계
-struct table : 
-    string col[];
-    string key_record[];
-
-*/
--- acBook_orderConverter.c
+++ acBook_orderConverter.c
@@ -1,1 +1,50 @@
 #include "acBook_orderConverter.h"
+
+Table* order_manager(char* order_string) {
+    Order_queue* order = (Order_queue*) malloc(sizeof(Order_queue));
+    order->order = NULL;
+    order->next = NULL;
+    Order_queue* order_head = &order;
+
+    char temp[100] = {0, };     // 문자열의 일부를 잘라 전달하기 위한 임시 문자 배열
+    int i = 0;
+    int tag = 0;        // 문자열이 잘린 시점을 임시로 저장할 태그.
+
+    for (i = 0; ; i++) {
+        if (order_string[tag + i] == '(') 
+            act_convert(order, temp, i, tag, 0);
+        else if (order_string[tag + i - 1] == ')') 
+            act_convert(order, temp, i, tag, 0);     
+        else if (order_string[tag + i] == ' ') 
+            act_convert(order, temp, i, tag, 1);
+        else 
+            temp[i] = order_string[tag + i];
+
+        if ( order_string[tag + i] != '\0')
+            break;
+    }
+
+    return table_manager(order_head);
+}
+
+// 반복되는 작업 내용을 추출한 중간다리 함수.
+// 변환 함수 호출 및 기타 작업을 수행.
+void act_convert(Order_queue* order, char* order_string, int* i, int* tag, int next_is_space) {
+    order_string[*i + 1] = "\0";
+    order = convert_str_to_order(order, order_string, *i + 1);
+    *tag = i;
+    if (next_is_space == 1) 
+        *tag += 1;
+    *i = -1;
+}
+
+Order_queue* convert_str_to_order(Order_queue* order, char* order_string, int string_length) {
+    Order_queue* new_order = (Order_queue*) malloc(sizeof(Order_queue));
+    order->next = &new_order;
+    new_order->next = NULL;
+
+    new_order->order = (char*) malloc(sizeof(char) * (string_length + 1));
+    new_order->order = order_string;
+
+    return &new_order;
+}
