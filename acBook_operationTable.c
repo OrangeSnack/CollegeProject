86bed1942980d1331584a8feff1a30932451794b
slient13 <sjyhc25@gmail.com>
2019-06-07 02:07:50 +0900

    테이블 연산부 정의 추가(미완성)

--- acBook_operationTable.c
+++ acBook_operationTable.c
@@ -1,24 +1,136 @@
 #include "acBook_operationTable.h"
 
 
-Table* table_manager(Order_queue* order) {
-    static char* list_table_name[50];           // 테이블 이름 리스트
-    static Table* list_table_address[50];       // 테이블 주소 리스트
+Table** table_manager(Order_queue* order) {
+    static Table_list main_table_list;      // 삭제 혹은 프로그램 종료 전까지 유지되는 테이블 리스트
+    static Table_list temp_table_list;      // 매 명령 실행 요청이 들어올 때마다 자동으로 삭제되는 임시 테이블 리스트
+    static Table_list return_table_list;    // 반환값으로 전달 될 테이블 리스트. 다른 리스트들의 값을 복사해 이용.
+    static int check_first_call = 0;        // 테이블 매니저의 첫 실행을 구분.
+
+    // 테이블 매니저 첫 호출 때 초기화 실시
+    if (check_first_call == 0) {
+        table_list_default_setting(&main_table_list, 50);
+        table_list_default_setting(&temp_table_list, 20);
+        table_list_default_setting(&return_table_list, 20);
+    }
+
+    // 임시 테이블 리스트 전체 초기화.
+    // 해당 리스트에 테이블이 남아있다면 그 테이블의 메모리를 삭제함.
+    for (int i = 0; i < temp_table_list.max_size; i++) {
+        if (temp_table_list.address[i] =! NULL) {
+            delete_table(temp_table_list.name[i]);
+            temp_table_list.address[i] = NULL;
+            free(temp_table_list.name[i]);
+        }
+    }
+
+    // 반환 테이블 리스트 전체 초기화.
+    // 해당 리스트에 테이블이 남아있더라도, 메모리는 조작하지 않음.
+    for (int i = 0; i < return_table_list.max_size; i++) {
+        if (return_table_list.address[i] != NULL) {
+            return_table_list.address[i] = NULL;
+            return_table_list.name[i] = NULL;
+        }
+    }
     
     // 명령 수행부.
+        // 열람할 테이블 지정.
     // 수행된 명령은 자동으로 메모리 반환.
 
-    // return table;
+    return return_table_list.address;
+}
+
+void table_list_default_setting(Table_list* target, int max_size) {
+    // 최대 사이즈 지정
+    target->max_size = max_size;
+    // 이름 배열 동적 할당. 동시에 초기화.
+    target->name = (char*) calloc(target->max_size, sizeof(char*));
+    // 테이블 주소 배열 동적 할당. 동시에 초기화.
+    target->address = (Table*) calloc(target->max_size, sizeof(Table*));
+    // 포함 테이블 갯수 초기화
+    target->num = 0;
 }
 
 
 
-Table* new_table(char* name);
+Table* new_table(char* name, Table_list* target) {
+    int index;
 
-void delete_table(Table* target);
+    // 이름 충돌 확인
+    for (index = 0; index < target->max_size; index++) {
+        if (strcmp(target->name, name) == 0) {
+            show_error_message("new_table()", 409);
+            return NULL;
+        }            
+    }
+
+    // 잔여 공간 확인
+    for (index = 0; ;index++) {
+        if (index == target->max_size) {
+            show_error_message("new_table()", 507);
+            return NULL;
+        }
+        else if (target->address[index] == NULL) {
+            break;
+        }
+    }
+
+    // 테이블 객체 동적 할당.
+    Table* table = (Table*) malloc(sizeof(Table));
+    
+    // 테이블 리스트 객체 정보 첨가
+    target->name[index] = (char*) malloc((strlen(name) + 1) * sizeof(char));
+    strcpy(target->name[index], name);
+    target->address[index] = table;
+
+    // 테이블 객체 초기화
+    strcpy(table->name, name);
+    table->start_col = NULL;
+    table->num_col = 0;
+    table->num_record = 0;
+    table->cursor = (Cursor*)malloc(sizeof(Cursor));
+
+    // 테이블 객체의 전용 커서 객체 초기화
+    table->cursor->pos_table = table;
+    table->cursor->pos_col = NULL;
+    table->cursor->pos_record = NULL;
+
+    return table;
+}
+
+void delete_table(Table* target, Table_list* main_list, Table_list* temp_list) {
+    
+    // 해당 테이블의 잔여 레코드, 컬럼 상태를 확인.
+        // 잔여 자료가 있다면 열, 레코드 삭제 함수를 반복 실행, 전부 말소.
+
+    move_cursor_default(target->cursor);
+    while(1) {
+        if (target->num_col == 0)
+            break;
+
+        delete_col(target->cursor);                
+    }
+    
+
+    // 모든 열이 사라지면 타겟 테이블 메모리 제거.
+}
 
 
-Col* new_col(Cursor* cursor, char* name);
+void set_table_temp(Table* target, Table_list* main_list, Table_list* temp_list) {
+
+}
+
+void set_table_main(Table* target, Table_list* main_list, Table_list* temp_list) {
+
+}
+
+
+Col* new_col(Cursor* cursor, char* name) {
+    // 열 객체 동적할당
+    // 해당 커서 다음 위치에 push
+    // 열 이름 기입.
+    // 커서를 이용해 Table 구조체에 접근해서 현재 레코드 갯수만큼 더미 데이터 라인 생성
+}
 
 void delete_col(Cursor* cursor, char* name);
 
@@ -40,8 +152,10 @@ void new_cursor();
 
 void delete_cursor();
 
-Cursor* move_cursor_by_colName(char* col_name);
+Cursor* move_cursor_by_colName(Cursor* target, char* col_name);
 
-Cursor* move_cursor_col(int direction);
+Cursor* move_cursor_col(Cursor* target, int direction);
 
-Cursor* move_cursor_record(int direction);
+Cursor* move_cursor_record(Cursor* target, int direction);
+
+Cursor* move_cursor_default(Cursor* target);
--- acBook_operationTable.h
+++ acBook_operationTable.h
@@ -17,11 +17,37 @@
 #ifndef acBook_operationTable
     #define acBook_operationTable
 
+    #include <stdlib.h>
+    #include <string.h>
+
     #include "acBook_processingData.h"      // 테이블 연산 파일
+    #include "acBook_error.h"               // 에러 처리 파일
 
     typedef struct _Order_queue Order_queue;
+    typedef struct _Table_list Table_list;
+    typedef struct _Table Table;
+    typedef struct _Col Col;
+    typedef struct _Data Data;
+    typedef struct _Cursor Cursor;
 
-    // 테이블 구조체
+    // 테이블 리스트 구조체
+    // char** name;
+    // Table** address;
+    // int num;
+    // int max_size;
+    typedef struct _Table_list {
+        char** name;            // 테이블의 이름.
+        Table** address;        // 테이블의 주소
+        int num;                // 해당 구조체에 포함되는 테이블의 갯수.
+        int max_size;           // 최대로 담을 수 있는 공간의 크기. 동적배열에 이용.
+    } Table_list;
+
+    // 개요 : 테이블 구조체
+    // - char* name;
+    // - Col* start_col;
+    // - int num_col;
+    // - int num_record;
+    // - Cursor* cursor;
     typedef struct _Table {
         char* name;         // 테이블의 이름
         Col* start_col;     // 시작열을 가리키는 포인터.
@@ -31,16 +57,39 @@
     } Table;
 
     // 조작 명령을 받아 테이블 연산을 실행해 반환.
-    Table* table_manager(Order_queue* order);
+    Table** table_manager(Order_queue* order);
+    // 기타 부속함수.
+    void table_list_default_setting(Table_list* target, int max_size);
+
     // 테이블 생성 함수.
     // name : 생성할 테이블의 이름
-    Table* new_table(char* name);
+    // target : 테이블을 생성할 위치(메인, 임시 리스트)
+    Table* new_table(char* name, Table_list* target);
     // 테이블 삭제 함수.
-    // target : 삭제할 테이블 객체의 주소
-    void delete_table(Table* target);
+    // target : 삭제할 테이블 주소
+    // main_list : 메인 테이블 리스트 
+    // temp_list : 임시 테이블 리스트 
+    void delete_table(Table* target, Table_list* main_list, Table_list* temp_list);
+    // 임시 테이블로 변경
+    // target : 변경시킬 테이블 주소
+    // main_list : 메인 테이블 리스트
+    // temp_list : 임시 테이블 리스트 
+    void set_table_temp(Table* target, Table_list* main_list, Table_list* temp_list);
+    // 메인 테이블로 변경
+    // 임시 테이블 지정.
+    // target : 변경시킬 테이블 주소
+    // main_list : 메인 테이블 리스트
+    // temp_list : 임시 테이블 리스트 
+    void set_table_main(Table* target, Table_list* main_list, Table_list* temp_list);
 
 
-    // 열 구조체
+    // 개요 : 열 구조체
+    // 비고 : 열 이름은 절대 중복될 수 없음.
+    // - char* name;
+    // - Col* next;
+    // - Col* prev;
+    // - Data* start_data;
+    // - int is_primaryKey;
     typedef struct _Col {
         char* name;         // 해당 열의 이름.
         Col* next;          // 다음열을 가리키는 포인터. 끝 열이면 NULL
@@ -49,22 +98,29 @@
         int is_primaryKey;  // 해당 열이 기준 열(primary key)인지 확인. 0 : 아님, 1 : 맞음.
     } Col;
 
-    // 열 생성 함수.
-    // 생성 위치는 항상 마지막 열 다음.
+    // 개요 : 열 생성 함수.
+    // 비고 : 처음 생성하는 열은 자동으로 'primary key'가 됨.
+    // 생성위치 : 현재 커서가 지정하고 있는 위치 다음.
     // 레코드가 있는 상태에서 열을 추가시키면, 그 열의 데이터는 NULL 으로 초기화.
     // name : 새로 생성할 열의 이름.
     Col* new_col(Cursor* cursor, char* name);
     // 열 삭제 함수.
     // name : 삭제할 열의 이름
     void delete_col(Cursor* cursor, char* name);
+    // 열 추가 함수.
+    // 생성위치 : 현재 커서가 지정하고 있는 위치 다음.
+    Col* add_col(Cursor* cursor, char* name, Data* data_start);
     // 열 이동 함수.
     // 커서가 가리키고 있는 열을 좌, 우 일정칸 움직임. 실행 후 0을 반환.
     // 만약 이동할 공간이 불충분하다면 실행하지 않고 -1을 반환.
     int move_col(Cursor* cursor, int move_length);
 
 
-    // 데이터 구조체. 
-    // 데이터 관련 조작은 단일 데이터 수정 제외하곤 전부 레코드 단위로 동작.
+    // 개요 : 데이터 구조체. 
+    // 비고 : 첫번째 데이터는 항시 head 역할을 위해 자동생성되며, 데이터를 저장하지 않음.
+    // - char* contents;  
+    // - Data* next;      
+    // - Data* prev;      
     typedef struct _Data {
         char* contents;     // 해당 위치에 기록된 데이터 내용.
         Data* next;         // 다음 레코드 데이터. 끝이면 NULL.
@@ -90,8 +146,11 @@
     void sort_data(Cursor* cursor, char** col_name, int direction);
 
 
-    // 검색, 삽입, 삭제 등을 시행할 때 관측하고 있는 위치를 기록하는 구조체
-    // 테이블 객체와 같이 생성되었다가, 같이 삭제됨.
+    // 개요 : 검색, 삽입, 삭제 등을 시행할 때 관측하고 있는 위치를 기록하는 구조체
+    // 비고 : 메인 커서는 테이블 객체에 포함되고, 기타 필요에 따라 보조 임시 객체 생성, 활용.
+    // - Table*  pos_table;  
+    // - Col*    pos_col;    
+    // - Data**  pos_record; 
     typedef struct _Cursor {
         Table*  pos_table;  // 현재 가리키고 있는 데이터의 테이블 주소
         Col*    pos_col;    // 현재 가리키고 있는 데이터의 열 주소
@@ -104,12 +163,14 @@
     void delete_cursor();
     // 커서 이동 함수 : 지정한 열로 이동
     // col_name : 이동할 열의 이름
-    Cursor* move_cursor_by_colName(char* col_name);
+    Cursor* move_cursor_by_colName(Cursor* target, char* col_name);
     // 커서 이동 함수 : 열 이동.
     // direction : 이동할 방향. 1 = 다음, -1 = 이전.
-    Cursor* move_cursor_col(int direction);
+    Cursor* move_cursor_col(Cursor* target, int direction);
     // 커서 이동 함수 : 행(레코드) 이동
     // direction : 이동할 방향. 1 = 다음, -1 = 이전.
-    Cursor* move_cursor_record(int direction);
+    Cursor* move_cursor_record(Cursor* target, int direction);
+    // 커서 이동 함수 : 첫번째 행, 헤더 레코드로 이동.
+    Cursor* move_cursor_default(Cursor* target);
 
 #endif
